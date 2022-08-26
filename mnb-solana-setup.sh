1ca4ce95a2d9736f2fb15b9ac33aacad80706e68
zeppski88 <86799896+zeppski88@users.noreply.github.com>
2022-08-26 22:12:30 +0300

    Update mnb-solana-setup.sh

--- mnb-solana-setup.sh
+++ mnb-solana-setup.sh
@@ -1,11 +1,11 @@
 #
-# Last update 05/01/2022  version 1.8.11
+# Last update 23/08/2022  version 1.10.35
 #
 ### Ставим оптимизацию CPU
 apt-get update && echo -e 'ENABLE="true"\nGOVERNOR="performance"' > /etc/default/cpufrequtils && apt-get install -y cpufrequtils moreutils && systemctl restart cpufrequtils.service && systemctl disable ondemand
 
 ### Install mainnet beta  (first install)
-curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.8.11/install/solana-install-init.sh | sh -s - v1.8.11
+curl -sSf https://raw.githubusercontent.com/solana-labs/solana/v1.10.35/install/solana-install-init.sh | sh -s - v1.10.35
 
 ### Экспортнуть PATH или перезайти в терминал
 export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
@@ -24,20 +24,20 @@ systemctl daemon-reload
 systemctl enable solana.service
 
 # Update
-export ver_install=1.8.11 && \
-solana-install init $ver_install && \
-unset ver_install && \
-systemctl restart solana-sys-tuner && \
-echo version upgraded, sys-tuner restarted, solana service NOT restarted
+# export ver_install=1.8.11 && \
+# solana-install init $ver_install && \
+# unset ver_install && \
+# systemctl restart solana-sys-tuner && \
+# echo version upgraded, sys-tuner restarted, solana service NOT restarted
 
 ### Исключаем solana-validator из rsyslog
 echo 'if $programname == "solana-validator" then stop' > /etc/rsyslog.d/01-solana-remove.conf && systemctl restart rsyslog
 
 ### Устанавливаем prometheus-node-exporter 0.17.0+ds-3+b11 just because it works with my current dashboard
-lsb_release -a 2>&1 | grep -q Ubuntu && \
-wget http://http.us.debian.org/debian/pool/main/p/prometheus-node-exporter/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb -O /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb && \
-dpkg -i /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb && \
-rm -rf /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb
+# lsb_release -a 2>&1 | grep -q Ubuntu && \
+# wget http://http.us.debian.org/debian/pool/main/p/prometheus-node-exporter/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb -O /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb && \
+# dpkg -i /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb && \
+# rm -rf /root/prometheus-node-exporter_0.17.0+ds-3+b11_amd64.deb
 
 ### Создаём tmpfs аккаунт 64GB
 #mkdir -p /root/solana/validator-ledger/accounts
@@ -55,12 +55,11 @@ cd /root/solana
 # Вариант А (просто переезд)
 ### создаём файлы ключей validator-keypair и vote-account-keypair
 # На старой ноде:
-cat /root/solana/validator-keypair.json
-cat /root/solana/vote-account-keypair.json
+# cat /root/solana/validator-keypair.json
+# cat /root/solana/vote-account-keypair.json
 
 # На новой ноде:
 touch validator-keypair.json && chmod 0600 validator-keypair.json && echo edit validator-keypair.json && sleep 5 && nano validator-keypair.json
-touch vote-account-keypair.json && chmod 0600 vote-account-keypair.json && echo edit vote-account-keypair.json && sleep 5 && nano vote-account-keypair.json
 
 ### Устанавливаем в конфиг сеть MN и ключ валидатора. 
 solana config set --url https://api.mainnet-beta.solana.com --keypair /root/solana/validator-keypair.json
@@ -94,7 +93,8 @@ solana-gossip spy --entrypoint mainnet-beta.solana.com:8001
 ###create vote account - https://docs.solana.com/running-validator/validator-start#create-vote-account
 solana-keygen new -o /root/solana/vote-account-keypair.json
 
-nano vote-account-keypair.json
+# nano vote-account-keypair.json
+# вывод 
 cat vote-account-keypair.json &&echo
 
 ### create vote account on blockchain
@@ -112,19 +112,19 @@ while true; do echo "______________ $(TZ=UTC date) ______________"; du -sh /root
 while true; do echo "______________ $(TZ=UTC date) ______________"; du -sh /root/solana/validator-ledger/ 2>/dev/null; du -sh /root/solana/validator-ledger/accounts*/ 2>/dev/null; timeout 30 solana catchup ~/solana/validator-keypair.json http://127.0.0.1:8899/ || echo timeout; timeout 30 solana block-production  --epoch $(solana epoch )  | grep -e " Identity Pubkey\|$(solana-keygen pubkey /root/solana/validator-keypair.json)"; for i in $(seq 1 6); do for j in $(seq 1 30); do echo -ne .; sleep 1; done; echo -ne $(( i * j )); done; echo ""; done
 
 ### create stake keypair - https://docs.solana.com/running-validator/validator-stake#create-stake-keypair
-solana-keygen new -o /root/solana/validator-stake-keypair.json
-cat validator-stake-keypair.json &&echo
+# solana-keygen new -o /root/solana/validator-stake-keypair.json
+# cat validator-stake-keypair.json &&echo
 
 ### delegate
-solana create-stake-account /root/solana/validator-stake-keypair.json 100
+###solana create-stake-account /root/solana/validator-stake-keypair.json 100
 
-solana delegate-stake /root/solana/validator-stake-keypair.json /root/solana/vote-account-keypair.json 
+### solana delegate-stake /root/solana/validator-stake-keypair.json /root/solana/vote-account-keypair.json 
 
 ### view your vote account:
-solana vote-account /root/solana/vote-account-keypair.json
+## solana vote-account /root/solana/vote-account-keypair.json
 
 ###View your stake account, the delegation preference and details of your stake:
-solana stake-account /root/solana/validator-stake-keypair.json
+## solana stake-account /root/solana/validator-stake-keypair.json
 
 solana validators | grep -e "$(solana-keygen pubkey /root/solana/validator-keypair.json)"
 
@@ -136,14 +136,14 @@ solana validator-info publish "<some name that will show up in explorer>" -n <ke
 ###solana validator-info publish "Elvis Validator" 
 
 ###
-echo "export SOLANA_ADDRESS=$(solana-keygen pubkey /root/solana/validator-keypair.json)" | tee -a ~/.bashrc 
-echo export SOLANA_VALIDATOR_INFO_PUBKEY=$(solana validator-info get | grep "$(solana-keygen pubkey /root/solana/validator-keypair.json)" -A5 | grep "Info Address" | head -n1 | awk '{ print $3}') | tee -a ~/.bashrc 
-echo export SOLANA_VOTE_ADDRESS=$(solana validators  --output json | jq -r '.currentValidators[]  | select(.identityPubkey == "'$SOLANA_ADDRESS'") | .voteAccountPubkey') | tee -a ~/.bashrc 
-source ~/.bashrc
-clear && solana stakes $SOLANA_VOTE_ADDRESS   ; echo identity balance: $(solana balance); echo vote balance: $(solana balance $SOLANA_VOTE_ADDRESS)   ; solana epoch-info   ; solana block-production   | grep -e " Identity\|$SOLANA_ADDRESS" ; solana validator-info get $SOLANA_VALIDATOR_INFO_PUBKEY   ; solana validators   | grep -e "Identity\|$SOLANA_ADDRESS" ; echo "running: "$(curl -s -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getVersion"}' http://localhost:8899 | jq -r '.result."solana-core"' ); echo "installed: "$(solana -V)
+#echo "export SOLANA_ADDRESS=$(solana-keygen pubkey /root/solana/validator-keypair.json)" | tee -a ~/.bashrc 
+#echo export SOLANA_VALIDATOR_INFO_PUBKEY=$(solana validator-info get | grep "$(solana-keygen pubkey /root/solana/validator-keypair.json)" -A5 | grep "Info Address" | head -n1 | awk '{ print $3}') | tee -a ~/.bashrc 
+#echo export SOLANA_VOTE_ADDRESS=$(solana validators  --output json | jq -r '.currentValidators[]  | select(.identityPubkey == "'$SOLANA_ADDRESS'") | .voteAccountPubkey') | tee -a ~/.bashrc 
+#source ~/.bashrc
+#clear && solana stakes $SOLANA_VOTE_ADDRESS   ; echo identity balance: $(solana balance); echo vote balance: $(solana balance $SOLANA_VOTE_ADDRESS)   ; solana epoch-info   ; solana block-production   | grep -e " Identity\|$SOLANA_ADDRESS" ; solana validator-info get $SOLANA_VALIDATOR_INFO_PUBKEY   ; solana validators   | grep -e "Identity\|$SOLANA_ADDRESS" ; echo "running: "$(curl -s -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getVersion"}' http://localhost:8899 | jq -r '.result."solana-core"' ); echo "installed: "$(solana -V)
 
 ###
 while true; do echo "______________ $(TZ=UTC date) ______________"; du -sh /root/solana/validator-ledger/ 2>/dev/null; du -sh /root/solana/validator-ledger/accounts*/ 2>/dev/null; timeout 30 solana catchup $SOLANA_ADDRESS http://127.0.0.1:8899/ || echo timeout; timeout 30 solana block-production    | grep -e " Identity \|$SOLANA_ADDRESS"; for i in $(seq 1 6); do for j in $(seq 1 30); do echo -ne .; sleep 1; done; echo -ne $(( i * j )); done; echo ""; done
 
 # monitoring
-wget https://raw.githubusercontent.com/mr0wnage/scripts/main/monitoring.sh -O ~/monitoring.sh && bash ~/monitoring.sh
+# wget https://raw.githubusercontent.com/mr0wnage/scripts/main/monitoring.sh -O ~/monitoring.sh && bash ~/monitoring.sh
